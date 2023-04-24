29c7dce17473894a3e591534805692fa5c78e9cc
ibrakhimk <kasymov_i@iuca.kg>
2023-04-24 16:09:21 +0600

    2.0

--- app/src/main/java/com/example/dz1_month6/MainActivity.kt
+++ app/src/main/java/com/example/dz1_month6/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.dz1_month6
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dz1_month6.databinding.ActivityMainBinding
 
@@ -10,26 +12,46 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                val text =result.data?.getStringExtra("text")
+                binding.etText.setText(text)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val intent: Intent =
-            intent
-        var name = intent.getStringExtra("key")
-        binding.etText.setText(name)
         binding.btnSend.setOnClickListener {
-            if (binding.etText.text.toString() == "") {
-                Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "С первого на второй", Toast.LENGTH_SHORT).show()
-                val questionIntent = Intent(
-                    this@MainActivity,
-                    MainActivity2::class.java
-                )
-                questionIntent.putExtra("key", binding.etText.text.toString())
-                startActivityForResult(questionIntent, 2)
+            if (binding.etText.text.isEmpty()){
+                Toast.makeText(this, "Поле не может быть пустым!", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, MainActivity2:: class.java)
+                intent.putExtra("text1", binding.etText.text.toString())
+                setResult(Activity.RESULT_OK, intent)
+                launcher.launch(intent)
             }
         }
     }
 }
+//        val intent: Intent =
+//            intent
+//        val name = intent.getStringExtra("key")
+//        binding.etText.setText(name)
+//        binding.btnSend.setOnClickListener {
+//            if (binding.etText.text.toString() == "") {
+//                Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "С первого на второй", Toast.LENGTH_SHORT).show()
+//                val questionIntent = Intent(
+//                    this@MainActivity,
+//                    MainActivity2::class.java
+//                )
+//                questionIntent.putExtra("key", binding.etText.text.toString())
+//                startActivityForResult(questionIntent, 2)
+//            }
+//        }
+//    }
+//}
--- app/src/main/java/com/example/dz1_month6/MainActivity2.kt
+++ app/src/main/java/com/example/dz1_month6/MainActivity2.kt
@@ -1,8 +1,10 @@
 package com.example.dz1_month6
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dz1_month6.databinding.ActivityMain2Binding
 
@@ -14,22 +16,32 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        val intent: Intent =
-            intent
-        var name = intent.getStringExtra("key")
-        binding.etText2.setText(name)
+        binding.etText2.setText(intent.getStringExtra("text1"))
+
         binding.btnSend2.setOnClickListener {
-            if (binding.etText2.text.toString() == "") {
-                Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "С первого на второй", Toast.LENGTH_SHORT).show()
-                val questionIntent = Intent(
-                    this@MainActivity2,
-                    MainActivity::class.java
-                )
-                questionIntent.putExtra("key", binding.etText2.text.toString())
-                startActivityForResult(questionIntent, 2)
-            }
+            val intent = Intent()
+            intent.putExtra("text", binding.etText2.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish()
         }
     }
 }
+//        val intent: Intent =
+//            intent
+//        val name = intent.getStringExtra("key")
+//        binding.etText2.setText(name)
+//        binding.btnSend2.setOnClickListener {
+//            if (binding.etText2.text.toString() == "") {
+//                Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "С вторгого на первый", Toast.LENGTH_SHORT).show()
+//                val questionIntent = Intent(
+//                    this@MainActivity2,
+//                    MainActivity::class.java
+//                )
+//                questionIntent.putExtra("key", binding.etText2.text.toString())
+//                startActivityForResult(questionIntent, 2)
+//            }
+//        }
+//    }
+//}
